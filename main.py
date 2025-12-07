301c3d65de79313d5f9b1b09289cb5eec128d3f4
Briinaa <22005187@gateshead.ac.uk>
2025-12-07 15:12:54 +0000

    Update main.py
    First part of the program: add numbers, count list items, and menu options.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ("hello world")
+def add(a, b):
+  return a+b
+
+def lenght_of_list(my list):
+  return len(my list)
+
+print("1: Add numbers")
+print("2: Count items")
+
+choice = input("choose 1 or 2")
+
