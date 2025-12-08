f4626db3eabae07f2be6fc4c2590296a9fc2876d
Briinaa <22005187@gateshead.ac.uk>
2025-12-08 11:59:10 +0000

    Python-Item-Counter
    A simple Python program that asks the user to enter items into a list and then counts and displays the total number of items.

--- main.py
+++ main.py
@@ -19,3 +19,9 @@ elif choice =="2":
  items = []
  i = 0
  
+ while i < 3:
+  item = input("enter item:")
+  items.append(item)
+  i = i + 1
+ total = length_of_list(items)
+ print("total items:", total)
