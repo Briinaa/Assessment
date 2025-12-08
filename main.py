daa78b44bb426fb1c69247c5a2650663b58afc24
Briinaa <22005187@gateshead.ac.uk>
2025-12-08 11:31:00 +0000

    Python-Add-Two-Numbers
    A simple Python program that asks the user for two numbers and prints the result of their addition.

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ print("2: Count items")
 
 choice = input("choose 1 or 2:")
 
+if choice =="1":
+ x = int(input("first number:"))
+ y = int(input("second number:"))
+ result = add(x, y)
+ print("result:", result)
+
