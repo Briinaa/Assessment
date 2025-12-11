47afb9913d7a957577e011dd60ed6e1213a01380
Briinaa <22005187@gateshead.ac.uk>
2025-12-11 12:40:18 +0000

    Add remove and change item features
    Added logic for options 3 and 4:
    - Option 3: create a list, ask which item to remove, remove it if found.
    - Option 4: create a list, ask which item to change, replace it with a new item.

--- main.py
+++ main.py
@@ -43,7 +43,24 @@ elif choice =="3":
   if remove_target in items:
     items.remove (remove_target)
     print(remove_target, "removed")
-    
+
+  else:
+    print("item not found.")
+  
+elif choice =="4":
+  items = []
+  i = 0
+  while i < 3:
+    item = input("enter item: ")
+    items.append(item)
+    i = i + 1
+  old = input("enter item to change: ")
+  if old in items:
+    new = input("enter new item: ")
+    index = items.index(old)
+    items[index] = new
+    print(old,"changed to", new)
+      
 else:
  print("invalid choice")
 
